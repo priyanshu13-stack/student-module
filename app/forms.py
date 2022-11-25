8830a22537b81420ed66077165fe83741582a8c1
Priyanshu <vermapriyanshu2000@gmail.com>
2022-11-25 22:32:14 +0530

    modifications in update form

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,29 @@
-from django.forms import ModelForm
 from .models import sample
+from django import forms
 
-class sampleform(ModelForm):
+class sampleform(forms.ModelForm):
     class Meta:
         model = sample
         fields = '__all__'
+        widgets = {
+            'sno': forms.TextInput(attrs={'class': 'form-control'}),
+            'enrollmentno': forms.TextInput(attrs={'class': 'form-control'}),
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'branch': forms.TextInput(attrs={'class': 'form-control'}),
+            'appno': forms.TextInput(attrs={'class': 'form-control'}),
+            'yearofadmission': forms.TextInput(attrs={'class': 'form-control'}),
+            'Fname': forms.TextInput(attrs={'class': 'form-control'}),
+            'Mname': forms.TextInput(attrs={'class': 'form-control'}),
+            'DOB': forms.TextInput(attrs={'class': 'form-control'}),
+            'gender': forms.TextInput(attrs={'class': 'form-control'}),
+            'category': forms.TextInput(attrs={'class': 'form-control'}),
+            'subcategory': forms.TextInput(attrs={'class': 'form-control'}),
+            'region': forms.TextInput(attrs={'class': 'form-control'}),
+            'rank': forms.TextInput(attrs={'class': 'form-control'}),
+            'allottedquota': forms.TextInput(attrs={'class': 'form-control'}),
+            'allottedcategory': forms.TextInput(attrs={'class': 'form-control'}),
+            'institute': forms.TextInput(attrs={'class': 'form-control'}),
+            'emailid': forms.TextInput(attrs={'class': 'form-control'}),
+            'address': forms.TextInput(attrs={'class': 'form-control'}),
+            'pcm': forms.TextInput(attrs={'class': 'form-control'}),
+        }
